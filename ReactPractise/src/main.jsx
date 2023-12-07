fe8aa102e54e991307bcad08f1e463142d2134b8
Monis Khan <khanmonis835@gmail.com>
2023-12-07 18:35:51 +0530

    learn about custom react a little bit

--- ReactPractise/src/main.jsx
+++ ReactPractise/src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+//create React Element
+// Render variable inside createElement
+const anotherElement= "Hyee From Asus Vivobook"
+const reactElement= React.createElement(
+  'a',{href:'https://google.com',target:'_blank'},'Please Click on Me',anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
+  // reactElement
     <App />
-  </React.StrictMode>,
+  
 )
